529d22f7860744164c9b6597f2f9ae94f2ff6632
Daniel Cruz López <danielcruzlopez1501@gmail.com>
2024-11-07 13:21:36 +0000

    game over logic

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ class Game:
         self.dt = 0
         self.score = 0
 
+        #self.background = pygame.image.load("assets/images/Ruined City Background Preview.png")
+
         self.player = Player(position_x=self.screen_width/2, position_y=700, width=100, height=20, colour="white")
         self.ball = Ball(position_x=self.screen_width / 2, position_y=self.screen_height / 2, speed_x=-300, speed_y=-300, radius=10, colour="white")
         self.colours = ["#000000", "#0D1B2A", "#1B263B", "#2A3C56", "#3E5C76", "#4F6D87", 
@@ -19,7 +21,7 @@ class Game:
           "#1A6A99", "#2D7AAB", "#3298B9", "#3BB3CC", "#40C3DB", "#5ED4E8", 
           "#79E1EB", "#A2E7F5", "#D0F1F9", "#D9EAF5"]
         self.colour_index = 0
-        self.lost = False
+        self.game_over = False
         self.font = pygame.font.SysFont('Arial', 40)
 
     def run(self):
@@ -43,9 +45,12 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.game_over:
+                    self.__init__()
 
     def update(self):
-        if not self.lost:
+        if not self.game_over:
             self.ball.check_screen_collision(screen_width=self.screen_width, screen_height=self.screen_height)
             if self.ball.check_player_collision(self.player.position.x, self.player.position.y):
                 self.ball.incremental()
@@ -54,7 +59,7 @@ class Game:
                     self.colour_index += 1
             self.ball.update(self.dt)
         if self.ball.check_lost(screen_height=self.screen_height):
-            self.lost = True
+            self.game_over = True
 
     def draw(self):
         self.screen.fill(self.colours[self.colour_index])
@@ -62,6 +67,9 @@ class Game:
         self.player.draw(self.screen)
         score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
         self.screen.blit(score_text, (10, 10))
+        if self.game_over:
+            game_over_text = self.font.render(f"Game Over, CLick To Restart", True, (255, 0, 0))
+            self.screen.blit(game_over_text, (100, 400))
 
     def score_increment(self):
         self.score += 1
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from game import Game
 pygame.init()
 pygame.mixer.init()
 
-pygame.mixer.music.load("31 Gateway Galaxy.mp3") 
+pygame.mixer.music.load("assets/audio/31 Gateway Galaxy.mp3") 
 pygame.mixer.music.play(-1) 
 
 if __name__ == "__main__":
